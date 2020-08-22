08bc0bd4fa7fc9f3c3549233f7fe46c69d77b5e7
walker <565828928@qq.com>
2020-08-22 21:21:57 +0800

    wechat bug fix

--- client_web/src/App.vue
+++ client_web/src/App.vue
@@ -4,7 +4,7 @@
         <span slot="left">
             <mt-button icon="back" @click="back"></mt-button>
         </span>
-        <div slot="right" @click="scan" v-show="ifSanjiangModule && isWeChat" style="display:flex;flex-direction:column;align-items:flex-end;">
+        <div slot="right" @click="scan" v-show="ifZiubaoPlatform && isWeChat" style="display:flex;flex-direction:column;align-items:flex-end;">
             <div style="display:flex;flex-direction:column;align-items:center;width:25px;margin-right:10px;">
                 <img :src="scanImg" style="width:28px;"/>
                 <span style="font-size:11px;margin-top:5px;">扫一扫</span>
@@ -31,19 +31,23 @@ export default {
             scanImg: scanImg,
             weChatAuthState: 0,
             isWeChat: isWeChat(),
-            ifSanjiangModule: false,
-            wechatAuthResponseData,
+            ifZiubaoPlatform: false,
         }
     },
 
-    watch: {
-        $route: function() {
-            this.checkIfShowScan();
-        }
-    },
 
     created() {
         try {
+            if(process.env.NODE_ENV === 'production') 
+            {
+                // check if need show
+                this.ifZiubaoPlatform = window.location.href.search(/zyb\.ziubao\.com/) >= 0;
+                if(!this.ifZiubaoPlatform)
+                {
+                    return;
+                }
+            }
+
             //
             wx.ready(() => {
                 this.Toast("微信授权成功");
@@ -56,6 +60,9 @@ export default {
             wx.error(res => {
                 this.Toast(`微信授权失败, ${JSON.stringify(res)}`);
             });
+
+            //
+            this.checkIfShowScan();
         } catch (e) {
             this.Toast("微信授权监听函数添加失败: " + e.toString());
         }
@@ -65,53 +72,34 @@ export default {
     methods: {
         checkIfShowScan() {
              try {
-                //
-                this.ifSanjiangModule = window.location.href.search(/\/sanjiang/) >= 0;
+                // already pass wechat auth
+                if(this.weChatAuthState == 1) {
+                    return;
+                }
 
-                // check if need show
-                if(!this.ifSanjiangModule)
+                // not wechat browser
+                if(!this.isWeChat)
                 {
                     return;
                 }
 
-                //
-                if(this.weChatAuthState == 1) {
-                    //
-                    return wx.config({
-                        debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-                        appId: config.appId, // 必填，公众号的唯一标识
-                        timestamp: this.wechatAuthResponseData.timestamp, // 必填，生成签名的时间戳
-                        nonceStr: this.wechatAuthResponseData.nonceStr, // 必填，生成签名的随机串
-                        signature: this.wechatAuthResponseData.signature,// 必填，签名
-                        jsApiList: ['scanQRCode'], // 必填，需要使用的JS接口列表
-                        fail: ({errMsg}) => {
-                            this.Toast("微信config调用失败: " + errMsg.toString());
-                        }
-                    });
-                }
-
+                // check if wechat js sdk load
                 if(!wx)
                 {
                     return this.Toast("微信js sdk导入失败");
                 }
 
-                if(!this.isWeChat)
-                {
-                    return;
-                }
+                //
+                this.weChatJsSdkAuth().catch(e => {
+                    this.Toast(`微信授权服务请求失败, ${e.toString()}`);
+                });
             } catch(e) {
                  this.Toast("微信授权预检查失败: " + e.toString());
             }
-            
-            //
-            this.weChatJsSdkAuth().catch(e => {
-                this.Toast(`微信授权服务请求失败, ${e.toString()}`);
-            });
         },
 
         async weChatJsSdkAuth()
         {
-            this.Toast(window.location.href);
             //
             const { statusCode, data, message } = await this.axios.weChatJsSdkAuth.get(`/wxShare/getJSSdkSignature.do?url=${btoa(window.location.href)}`);
             
@@ -120,19 +108,15 @@ export default {
             {
                 await Promise.reject(message);
             }
-            
+
             //
-            this.wechatAuthResponseData = data;
             wx.config({
                 debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                 appId: config.appId, // 必填，公众号的唯一标识
-                timestamp: this.wechatAuthResponseData.timestamp, // 必填，生成签名的时间戳
-                nonceStr: this.wechatAuthResponseData.nonceStr, // 必填，生成签名的随机串
-                signature: this.wechatAuthResponseData.signature,// 必填，签名
+                timestamp: data.timestamp, // 必填，生成签名的时间戳
+                nonceStr: data.nonceStr, // 必填，生成签名的随机串
+                signature: data.signature,// 必填，签名
                 jsApiList: ['scanQRCode'], // 必填，需要使用的JS接口列表
-                fail: ({errMsg}) => {
-                    this.Toast("微信config调用失败: " + errMsg.toString());
-                }
             });
         },
 
@@ -140,26 +124,23 @@ export default {
         {
             if(this.weChatAuthState !== 1)
             {
-                //
-                this.weChatJsSdkAuth().catch(e => {
-                    this.Toast(`微信授权服务请求失败, ${e.toString()}`);
-                });
+                return this.Toast("微信授权失败,无法使用扫一扫功能,请刷新页面或者使用微信自带的扫一扫");
             }
 
             //
             try {
                 wx.scanQRCode({
-                        desc: 'scanQRCode desc',
-                        needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
-                        scanType: ["qrCode", "barCode"], // 可以指定扫二维码还是一维码，默认二者都有
-                        success: function ({resultStr}) {
+                    desc: 'scanQRCode desc',
+                    needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+                    scanType: ["qrCode", "barCode"], // 可以指定扫二维码还是一维码，默认二者都有
+                    success: function ({resultStr}) {
 
-                            // just jump
-                            window.location.href = resultStr;              
-                        },
-                        fail: ({errMsg}) => {
-                            this.Toast("微信扫一扫接口调用失败: " + errMsg.toString())
-                        }
+                        // just jump
+                        window.location.href = resultStr;              
+                    },
+                    fail: ({errMsg}) => {
+                        this.Toast("微信扫一扫接口调用失败: " + errMsg.toString())
+                    }
                 });
             } catch (e) {
                 this.Toast("微信扫一扫接口调用异常: " + e.toString());
