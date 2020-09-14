efb807a862294bd52cc2c5e69543013691f43e78
unknown <565828928@qq.com>
2020-09-14 16:45:39 +0800

    add config

--- client_web/globals.js
+++ client_web/globals.js
@@ -1,7 +1,18 @@
 process.env.ziubao_invoice_platform = "ziubao";
 
 //
-const rootPath = process.env.ziubao_invoice_platform == "zlkc" ? "/zlkc" : "/invoice/main";
+let rootPath = ""
+if(process.env.ziubao_invoice_platform == "zlkc") {
+    rootPath = "/zlkc";
+}
+else if (process.env.ziubao_invoice_platform == "ziubao")
+{
+    rootPath = "/invoice/main";
+}
+else
+{
+    rootPath = "/yingqiyu_test";
+}
 
 //
 module.exports = {
