72fc9872598961b9b17eebd0507317ad36d8d68a
ChineseFish <565828928@qq.com>
2021-03-10 14:59:22 +0800

    style change

--- client_web_manager/src/views/Main.vue
+++ client_web_manager/src/views/Main.vue
@@ -1,10 +1,10 @@
 <template>
-    <div style="text-align:center;width:100%;height:100%;">
+    <div style="text-align:center;width:100%;height:100%;display:flex;flex-direction:column;">
         <div class="bubble">
             <p>电子发票统计系统</p>
         </div>
 
-        <div style="overflow:scroll;height:calc(100% - 50px);">
+        <div style="overflow:auto;height:100%;background:#ffffff;box-sizing: border-box;padding:10px;margin:0px 10px;">
             <button class="button" @click="$router.push({path: 'sanjiang'})">三江码头</button>
             <button class="button" @click="$router.push({path: 'changzhiVehiclePark'})">长峙岛停车场</button>
             <button class="button" @click="$router.push({path: 'hxFerryShop'})" >海峡轮渡小卖部</button>
@@ -29,7 +29,7 @@ export default {
 <style lang="scss" scoped>
 
 .button {
-    width: 90vw;
+    width: 100%;
     height: 40px;
     margin: 10px 0px;
 }
@@ -38,6 +38,10 @@ export default {
     width: 100%;
     height: 50px;
     display: table;
+    background: #ffffff;
+    padding: 10px;
+    margin: 10px;
+    box-sizing: border-box;
 }
 
 .bubble p {
